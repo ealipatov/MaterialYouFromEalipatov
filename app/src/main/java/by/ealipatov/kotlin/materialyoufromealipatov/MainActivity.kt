e1ef31335cc7717bfa05791080fff205de0bb7d1
Evgeniy Alipatov <ealipatov@gmail.com>
2022-08-26 12:59:14 +0300

    Урок 2. Доработан метод смены и отображения выбраной темы.

--- app/src/main/java/by/ealipatov/kotlin/materialyoufromealipatov/MainActivity.kt
+++ app/src/main/java/by/ealipatov/kotlin/materialyoufromealipatov/MainActivity.kt
@@ -3,16 +3,20 @@ package by.ealipatov.kotlin.materialyoufromealipatov
 
 import android.content.Context
 import android.content.SharedPreferences
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import by.ealipatov.kotlin.materialyoufromealipatov.databinding.ActivityMainBinding
+import by.ealipatov.kotlin.materialyoufromealipatov.utils.SELECTED_THEME
 import by.ealipatov.kotlin.materialyoufromealipatov.utils.SHARED_PREF_FILE
 import by.ealipatov.kotlin.materialyoufromealipatov.utils.THEME_KEY
 import by.ealipatov.kotlin.materialyoufromealipatov.view.PictureOfTheDayFragment
 
 
 class MainActivity : AppCompatActivity() {
+
+    private var themeID = R.style.Theme_MaterialYouFromEalipatov
 
     override fun onCreate(savedInstanceState: Bundle?) {
         val binding = ActivityMainBinding.inflate(layoutInflater)
@@ -24,13 +28,24 @@ class MainActivity : AppCompatActivity() {
                 .replace(R.id.container, PictureOfTheDayFragment.newInstance()).commit()
         }
         val sharedPreferences: SharedPreferences = this.getSharedPreferences(SHARED_PREF_FILE, Context.MODE_PRIVATE)
-        if(savedInstanceState != null){
-            setTheme(sharedPreferences.getInt(THEME_KEY,R.style.Theme_MaterialYouFromEalipatov))
+        if(sharedPreferences.getBoolean(SHARED_PREF_FILE, false)) {
+            themeID = sharedPreferences.getInt(THEME_KEY,R.style.Theme_MaterialYouFromEalipatov)
         }
+
+        setTheme(themeID)
 
         //Ичпользуем системную тему
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.getDefaultNightMode())
         setTheme(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
     }
 
+    override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
+        outState.putInt(SELECTED_THEME, themeID)
+        super.onSaveInstanceState(outState, outPersistentState)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        themeID = savedInstanceState.getInt(SELECTED_THEME)
+    }
 }
--- app/src/main/java/by/ealipatov/kotlin/materialyoufromealipatov/view/SettingFragment.kt
+++ app/src/main/java/by/ealipatov/kotlin/materialyoufromealipatov/view/SettingFragment.kt
@@ -3,22 +3,31 @@ package by.ealipatov.kotlin.materialyoufromealipatov.view
 
 import android.content.Context
 import android.content.SharedPreferences
+import android.content.res.ColorStateList
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import by.ealipatov.kotlin.materialyoufromealipatov.R
 import by.ealipatov.kotlin.materialyoufromealipatov.databinding.FragmentSettingBinding
+import by.ealipatov.kotlin.materialyoufromealipatov.utils.SELECTED_THEME
 import by.ealipatov.kotlin.materialyoufromealipatov.utils.SHARED_PREF_FILE
 import by.ealipatov.kotlin.materialyoufromealipatov.utils.THEME_KEY
 import by.ealipatov.kotlin.materialyoufromealipatov.utils.toast
 import com.google.android.material.chip.Chip
+import kotlinx.android.synthetic.main.fragment_setting.*
 
 class SettingFragment: Fragment() {
 
     private var _binding: FragmentSettingBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var sharedPreferences: SharedPreferences
+    private lateinit var themeName: String
+    private var themeSelect: Int = 0
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,36 +35,71 @@ class SettingFragment: Fragment() {
     ): View {
         _binding = FragmentSettingBinding.inflate(inflater, container, false)
         return binding.root
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        sharedPreferences = requireActivity().getSharedPreferences(SHARED_PREF_FILE,
+            Context.MODE_PRIVATE)
+
+        themeSelect = sharedPreferences.getInt(THEME_KEY,0)
+
+        when(themeSelect){
+            R.style.MoonTheme -> {
+                themeName = getString(R.string.check_moon_theme)
+                moon_theme.isCheckable = false
+                binding.moonTheme.chipBackgroundColor = ColorStateList.valueOf(ContextCompat.getColor(requireContext(), R.color.green))
+                binding.selectedThemeName.text = themeName
+            }
+            R.style.MarsTheme -> {
+                themeName = getString(R.string.check_mars_theme)
+                mars_theme.isCheckable = false
+                binding.marsTheme.chipBackgroundColor = ColorStateList.valueOf(ContextCompat.getColor(requireContext(), R.color.green))
+                binding.selectedThemeName.text = themeName
+            }
+        }
 
         binding.chipThemeGroup.setOnCheckedStateChangeListener { chipGroup, position ->
             chipGroup.findViewById<Chip>(position[0])?.let {
                 when(it.id){
                     R.id.moon_theme ->{
-                        selectTheme(R.style.MoonTheme)
-                        toast("Выбрана лунная тема")
+                        if(themeSelect != R.style.MoonTheme){
+                            selectTheme(R.style.MoonTheme)
+                            toast(getString(R.string.check_moon_theme))
+                        }
                     }
                     R.id.mars_theme ->{
-                        selectTheme(R.style.MarsTheme)
-                        toast("Выбрана марсианская тема")
+                        if(themeSelect != R.style.MarsTheme){
+                            selectTheme(R.style.MarsTheme)
+                            toast(getString(R.string.check_mars_theme))
+                        }
                     }
                 }
             }
         }
     }
 
+    /***
+     * Функция смены темы. Сохраняем темы с помощью sharedPreferences и при перезапуске
+     * активити считывавем выбранную тему и применяем ее.
+     */
     private fun selectTheme(theme: Int){
-        val sharedPreferences = requireActivity().getSharedPreferences(SHARED_PREF_FILE,
-            Context.MODE_PRIVATE)
         val editor:SharedPreferences.Editor = sharedPreferences.edit()
         editor.putInt(THEME_KEY, theme)
+        editor.putBoolean(SHARED_PREF_FILE, true)
         editor.apply()
         requireActivity().recreate()
         requireActivity().supportFragmentManager.popBackStack()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putString(SELECTED_THEME, themeName)
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onViewStateRestored(savedInstanceState: Bundle?) {
+        super.onViewStateRestored(savedInstanceState)
+        themeName = savedInstanceState?.getString(SELECTED_THEME).toString()
     }
 
     override fun onDestroyView() {
