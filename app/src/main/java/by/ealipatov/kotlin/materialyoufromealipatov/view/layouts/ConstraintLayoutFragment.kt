f3a6ce047816f227fd5f70ff1e7f27fff2bd943c
Evgeniy Alipatov <ealipatov@gmail.com>
2022-10-18 01:43:26 +0300

    Урок 4. Макеты
    Попробовали группы

--- app/src/main/java/by/ealipatov/kotlin/materialyoufromealipatov/view/layouts/ConstraintLayoutFragment.kt
+++ app/src/main/java/by/ealipatov/kotlin/materialyoufromealipatov/view/layouts/ConstraintLayoutFragment.kt
@@ -5,15 +5,33 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import by.ealipatov.kotlin.materialyoufromealipatov.R
+import by.ealipatov.kotlin.materialyoufromealipatov.databinding.FragmentConstraintLayoutBinding
 
 class ConstraintLayoutFragment: Fragment() {
+
+    private var flag = false
+    private var _binding: FragmentConstraintLayoutBinding? = null
+    private val binding: FragmentConstraintLayoutBinding
+        get() {
+            return _binding!!
+        }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_constraint_layout, container, false)
+    ): View {
+        _binding = FragmentConstraintLayoutBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnGroupOnOff.setOnClickListener {
+            flag = !flag
+            binding.groupBtn.visibility = if (flag) View.GONE else View.VISIBLE
+        }
+
     }
 }
