803098a8cacee67e3c204626da80a9a5f783e826
Evgeniy Alipatov <ealipatov@gmail.com>
2022-10-21 21:53:59 +0300

    Урок 4. Макеты
    Добавили в FABBehavior.kt реакцию кнопки на изменение bottom_sheet

--- app/src/main/java/by/ealipatov/kotlin/materialyoufromealipatov/view/behavior/FABBehavior.kt
+++ app/src/main/java/by/ealipatov/kotlin/materialyoufromealipatov/view/behavior/FABBehavior.kt
@@ -5,6 +5,7 @@ import android.util.AttributeSet
 import android.view.View
 import android.widget.Button
 import androidx.coordinatorlayout.widget.CoordinatorLayout
+import by.ealipatov.kotlin.materialyoufromealipatov.R
 import com.google.android.material.appbar.AppBarLayout
 import java.lang.StrictMath.abs
 
@@ -16,7 +17,7 @@ class FABBehavior(context: Context, attrs: AttributeSet?=null):
         child: Button,
         dependency: View
     ): Boolean {
-        return (dependency is AppBarLayout)
+        return (dependency is AppBarLayout || dependency.id == R.id.bottom_sheet_container)
     }
 
 
@@ -30,6 +31,10 @@ class FABBehavior(context: Context, attrs: AttributeSet?=null):
             child.y = dependency.y + dependency.height - child.height/2
 
             child.alpha = 1 - (abs(dependency.y)/(dependency.height/2))
+        } else if (dependency.id == R.id.bottom_sheet_container) {
+            child.x = (dependency.width - child.width).toFloat()
+            child.y = dependency.y - child.height/2
+            child.alpha = (abs(dependency.y))
         }
         return super.onDependentViewChanged(parent, child, dependency)
     }
